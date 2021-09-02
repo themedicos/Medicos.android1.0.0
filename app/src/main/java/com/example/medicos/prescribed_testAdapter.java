cf0bab7f42b84778f218198aaf569af9ff55ef22
Arya_prakash <aryaprakash0406@gmail.com>
2021-09-02 17:01:53 +0700

    preview added

---
+++ app/src/main/java/com/example/medicos/prescribed_testAdapter.java
@@ -0,0 +1,47 @@
+package com.example.medicos;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class prescribed_testAdapter extends RecyclerView.Adapter<prescribed_testAdapter.prescribed_testView> {
+    ArrayList<prescribed_test> test_list_added = new ArrayList<>();
+
+    public prescribed_testAdapter(ArrayList<prescribed_test> test_list) {
+        this.test_list_added = test_list;
+    }
+
+    @NonNull
+    @Override
+    public prescribed_testView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.advice_test_box,parent,false);
+        return new prescribed_testView(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull prescribed_testView holder, int position) {
+        prescribed_test prescribed_test = test_list_added.get(position);
+        holder.new_test_name.setText(prescribed_test.getTest_name());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return test_list_added.size();
+    }
+
+    public class prescribed_testView extends  RecyclerView.ViewHolder{
+
+        TextView new_test_name;
+        public prescribed_testView(@NonNull View itemView) {
+            super(itemView);
+            new_test_name =(TextView)itemView.findViewById(R.id.new_test_name);
+        }
+    }
+}
